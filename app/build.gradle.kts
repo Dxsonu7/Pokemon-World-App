0c7b378bb6d7494df2c7d1eb4a0bfe86ca12cba3
Sonu Gupta <sonu72752@gmail.com>
2023-10-16 22:44:00 -0500

    implemented Pokemon API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.project_api"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.project_api"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,6 +37,11 @@ android {
 
 dependencies {
 
+    //    Adding glide and codepath libraries
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("com.codepath.libraries:asynchttpclient:2.2.0")
+    implementation ("com.codepath.libraries:asynchttpclient:2.2.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
--- app/src/main/java/com/example/project_api/MainActivity.kt
+++ app/src/main/java/com/example/project_api/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.project_api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    private fun getPokemonImageURL() {
+        val client = AsyncHttpClient()
+        client["https://pokeapi.co/api/v2/pokemon/", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Pokemon", "response successful")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Pokemon Error", errorResponse)
+            }
+        }]
+    }
+
+
 }
